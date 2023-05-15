9683ab4adf1604b497a1446bdd9c695d3b692126
MOONSTAR <aliayyildiz4642@gmail.com>
2023-05-15 22:22:09 +0300

    Portfolio-2 Flask02-03

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
@@ -13,5 +14,31 @@ def third():
 def forth(id):
     return f'Id of this page is {id}'
 if __name__ == '__main__':
+=======
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    return 'Hello world Oktay'
+
+
+@app.route('/second')
+def second():
+    return 'This is second page'
+
+@app.route('/third')
+def third():
+    return 'This is third page'
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'Id of this page is {id}'
+
+
+if __name__ == '__main__':
+
+>>>>>>> 4231baa69c0f9c0c07dea11a8cb8b265ed9f8327
     # app.run(debug=True)
     app.run(host= '0.0.0.0', port=80)
--- flask-02-jinja-template/jinja.py
+++ flask-02-jinja-template/jinja.py
@@ -1,13 +1,34 @@
 from flask import Flask, render_template
+<<<<<<< HEAD
 app = Flask(__name__)
 @app.route('/')
 def head():
     return render_template('index.html', number1=112500, number2=225200)
 @app.route('/mult')
+=======
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def head():
+    return render_template('index.html', number1=888, number2=9999)
+
+
+@app.route('/mult')
+
+>>>>>>> 4231baa69c0f9c0c07dea11a8cb8b265ed9f8327
 def number():
     x=15
     y=20
     return render_template('body.html', num1=x, num2=y, mult=x*y)
+<<<<<<< HEAD
 if __name__ == '__main__':
     # app.run(debug=True)
     app.run(host= '0.0.0.0', port=80)
+=======
+
+if __name__ == '__main__':
+    # app.run(debug=True)
+    app.run(host= '0.0.0.0', port=80)
+>>>>>>> 4231baa69c0f9c0c07dea11a8cb8b265ed9f8327
